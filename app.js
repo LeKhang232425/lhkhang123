d6c60e071541ccb0101950a96fdf23ee488ce728
khang <hoangkhang2324@gmail.com>
2022-09-11 00:19:54 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
+app.get("/", (req, res) => {
+    res.json({ message: "Welcom to contact book application" });
+
+});
+
 // handle 404 response
 app.use((req, res, next) => {
     // Code ở đây sẽ chạy khi không có route được định nghĩa nào
@@ -23,9 +28,6 @@ app.use((err, req, res, next) => {
     message: error.message || "Internal Server Error",
     });
 });
-app.get("/", (req, res) => {
-    res.json({ message: "Welcom to contact book application" });
 
-});
 
 module.exports = app;
