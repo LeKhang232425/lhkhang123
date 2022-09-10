7ecfda5795ddeada6546261e9c8e14cf5bb47496
khang <hoangkhang2324@gmail.com>
2022-09-10 23:26:33 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//const contactsRouter = require("./app/routes/contact.route");
 app.get("/", (req, res) => {
     res.json({ message: "Welcom to contact book application" });
 
 });
+//app.use("/api/contacts", contactsRouter);
 module.exports = app;
